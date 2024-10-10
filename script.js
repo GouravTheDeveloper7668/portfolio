b83f35f7f38c4f1e1341d98a74cae63e27ddb15e
Gourav Vishwkarma <118762710+GouravTheDeveloper7668@users.noreply.github.com>
2024-10-10 17:45:32 +0530

    Changes One

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Scroll Animation
-window.addEventListener('scroll', function() {
-    let elements = document.querySelectorAll('.project');
-    let windowHeight = window.innerHeight;
-  
-    elements.forEach(function(element) {
-      let position = element.getBoundingClientRect().top;
-      
-      if (position < windowHeight - 100) {
-        element.style.transform = 'translateY(0)';
-        element.style.opacity = '1';
-      } else {
-        element.style.transform = 'translateY(100px)';
-        element.style.opacity = '0';
-      }
-    });
-  });
-  
+document.querySelector('.mobile-menu-btn').addEventListener('click', function() {
+  document.querySelector('.nav-links').classList.toggle('active');
+});
